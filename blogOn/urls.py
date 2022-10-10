c3a717c5b94256a4a22f3f77d400ada286c87c36
nitishchaudhary <006nschaudhary006@gmail.com>
2022-10-10 18:01:48 +0530

    share email

--- blogOn/urls.py
+++ blogOn/urls.py
@@ -4,5 +4,6 @@ from blogOn import views
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('blog_id=<int:pk>', views.blogDetail, name = 'blogDetail'),
-    path('comment_id=<int:pk>', views.comment, name = 'comment')
+    path('comment_id=<int:pk>', views.comment, name = 'comment'),
+    path('sharePost_id=<int:pk>', views.sharePost, name = 'sharePost'),
 ]
--- blogOn/views.py
+++ blogOn/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth.models import User
 from .models import Blog, Comment
 from django.contrib.auth import authenticate
+from django.core.mail import send_mail
+from django.conf import settings
 
 # Create your views here.
 
@@ -35,5 +37,25 @@ def comment(request, pk):
     else:
         return NULL
 
+def sharePost(request, pk):
+    if request.method == "POST":
+        currentBlog = Blog.objects.get(id = pk)
+        username = request.POST['username']
+        emailFrom = request.POST['emailFrom']
+        emailTo = request.POST['emailTo']
+        emailComments = request.POST['emailComments']
+        if emailFrom and emailTo:
+            send_mail(subject = currentBlog.title,
+            message = currentBlog.description,
+            from_email = settings.EMAIL_HOST_USER,
+            recipient_list= [emailTo]
+            )
+
+    elif request.method == "GET":
+        currentBlog = Blog.objects.get(id = pk)
+        if currentBlog is not None:
+            return render(request, 'sharePost.html', {'blog':currentBlog})
+        else:
+            return redirect('/')
         
     
